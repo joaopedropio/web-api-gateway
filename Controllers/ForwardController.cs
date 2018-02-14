9343ea77b01447ceb85f1f3a168d2b824efba3fb
Joao Pedro Pio <joao.pio@agilecontent.com>
2018-02-14 19:22:23 -0200

    Huge refactoring

--- Controllers/ForwardController.cs
+++ Controllers/ForwardController.cs
@@ -1,62 +1,32 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 
 namespace WebAPIGateway.Controllers {
     
-    [Route("/{*uri}")]
+    [Route("/{service}/{*uri}")]
     public class ForwardController : Controller 
     {
         private IDistributedCache _cache;
+        private HttpClient _client = new HttpClient();
         public ForwardController(IDistributedCache cache)
         {
             cache.SetString("user", "http://localhost:3000");
             _cache = cache;
         }
 
-        public string Index() 
+        public async Task<string> Index(string service, string uri)
         {
-            var uri = Request.Path.Value;
-            var service = ParseServiceFromRequest(uri);
-            var serviceDomain = GetServiceURL(service);
-            
-            if(serviceDomain == null || serviceDomain == "")
+            string serviceUrl = await _cache.GetStringAsync(service);
+
+            if(serviceUrl == null || serviceUrl == "")
                 return "There is no such service, you dumbass!";
             else
-                return APIResponse(serviceDomain + ParseServiceURI(uri));
-        }
-
-        private string ParseServiceFromRequest(string uri)
-        {
-            return uri.Split('/')[1];
-        }
-
-        private string GetServiceURL(string service)
-        {
-            return _cache.GetString(service);
-        }
-
-        private string ParseServiceURI(string uri)
-        {
-            var service = ParseServiceFromRequest(uri);
-            var serviceURI = uri.Remove(0, service.Length + 1);            
-            return serviceURI;
-        }
-
-        private string APIResponse(string uri)
-        {
-            var httpRequest = HttpWebRequest.CreateHttp(uri);
-            
-            using(var httpResponse = httpRequest.GetResponse()){
-
-                var stream = httpResponse.GetResponseStream();
-                var response = new StreamReader(stream);
-                var result = response.ReadToEnd();
-                return result;
-    
-            }
+                return await _client.GetStringAsync(serviceUrl + "/" + uri);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# Web API Gateway
+
+The Web API Gateway is a Dotnet Core API is the gateway to the microservices of the Social Movie project. 
+This gateway is only for web apps use. Soon, we'll have the Mobile API Gateway service to mobile clients.
+
+Environment variables
+* REDIS_IP: ip or domain of the Redis instance
