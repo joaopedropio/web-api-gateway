948c428d2a30d7957afd9fb1bb0f105c8eeb7a7a
Joao Pedro Pio <joao.pio@agilecontent.com>
2018-02-16 14:38:04 -0200

    Admin functions added, but the project needs a deep and serious refactoring

--- Controllers/ForwardController.cs
+++ Controllers/ForwardController.cs
@@ -25,25 +25,41 @@ namespace WebAPIGateway.Controllers {
         [Route("/{service}/{*uri}")]
         public async Task<IActionResult> Index(string service, string uri)
         {
+            if(service.ToLower() == "admin")
+            {
+                switch(Request.Method)
+                {
+                    case "GET" : return Ok(JObject.Parse(await GetAdmin(uri)));
+                    case "POST": return Ok(JObject.Parse(await PostAdmin(Request.Body)));
+                    case "DELETE": return Ok(JObject.Parse(await DeleteAdmin(uri)));
+                    default: return Ok("deu Ruim");
+                }
+            }
             string serviceUrl = await _cache.GetStringAsync(service);
-            if(serviceUrl == null) {
+            if(serviceUrl == null)
+            {
                 return Ok(JObject.Parse("{ \"error\": \"Invalid service\" }"));
             }
             string url = serviceUrl + "/" + uri;
 
-            switch(Request.Method) {
+            switch(Request.Method)
+            {
                 case "GET": return await ForwardGet(url);
                 case "POST": return await ForwardPost(url, Request.Body);
                 case "DELETE": return await ForwardDelete(url);
+                case "PUT": return await ForwardPut(url, Request.Body);
                 default: return Ok(JObject.Parse("{ \"status\": \"Method not valid!\" }"));
             }
         }
-        public async Task<IActionResult> ForwardGet(string url) {
+        public async Task<IActionResult> ForwardGet(string url)
+        {
             string response = await _client.GetStringAsync(url);
-            try {
+            try
+            {
                 return Ok(JArray.Parse(response));
             }
-            catch {
+            catch
+            {
                 return Ok(JObject.Parse(response));
             }
         }
@@ -58,6 +74,21 @@ namespace WebAPIGateway.Controllers {
             var response = await _client.DeleteAsync(url);
             return NoContent();
         }
+
+        public async Task<IActionResult> ForwardPut(string url, Stream bodyStream)
+        {
+            string body = await ParseBodyAsync(bodyStream);
+
+            return await PutBodyAsync(url, body);
+        }
+
+        public async Task<IActionResult> PutBodyAsync(string url, string body)
+        {
+            var content = new StringContent(body, UnicodeEncoding.UTF8, "application/json");
+            var response = await _client.PutAsync(url, content);
+            return Ok(JObject.Parse(await response.Content.ReadAsStringAsync()));
+        }
+
         public async Task<IActionResult> PostBodyAsync(string url, string body)
         {
             var content = new StringContent(body, UnicodeEncoding.UTF8, "application/json");
@@ -69,6 +100,55 @@ namespace WebAPIGateway.Controllers {
             using (StreamReader reader = new StreamReader(bodyStream, Encoding.UTF8))
             {  
                 return await reader.ReadToEndAsync();
+            }
+        }
+
+        public async Task<string> GetAdmin(string service)
+        {
+            if(service == null)
+            {
+                return "{ \"error\": \"no service provided\" }";
+            }
+            string url = await _cache.GetStringAsync(service);
+            if (url == "" || url == null)
+            {
+                return "{ \"error\": \"service not found\" }";
+            }
+            return "{\"service\": \"" + service + "\", \"url\": \"" + url + "\" }";
+        }
+
+        public async Task<string> PostAdmin(Stream bodyStream)
+        {
+            var bodyString = await ParseBodyAsync(bodyStream);
+            JObject body = JObject.Parse(bodyString);
+
+            try
+            {
+                string service = (string)body.GetValue("service");
+                string url = (string)body.GetValue("url");
+
+                await _cache.SetStringAsync(service, url);
+                return "{ \"status\": \"Service Added!\" }";
+            }
+            catch (System.Exception)
+            {
+                return "{ \"error\": \"Body cant be empty\" }"; 
+            }
+        }
+
+        public async Task<string> DeleteAdmin(string service)
+        {
+            if(service == null || service == "")
+            {
+                return "{ \"error\": \"servic can not be empty!\" }"; 
+            }
+            string value = await _cache.GetStringAsync(service);
+            if(value == null || value == "")
+            {
+                return "{ \"error\": \"service does not exist\" }"; 
+            } else {
+                await _cache.RemoveAsync(service);
+                return "{ \"status\": \"service deleted!\" }"; 
             }
         }
     }
